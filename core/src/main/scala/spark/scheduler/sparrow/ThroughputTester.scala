1a73afd7711fa6f48337c0c76c462bb15a97a662
Kay Ousterhout <kayo@yahoo-inc.com>
2013-09-06 16:34:00 -0700

    Added Spark app that launches tasks to test scheduler throughput

---
+++ core/src/main/scala/spark/scheduler/sparrow/ThroughputTester.scala
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spark.scheduler.sparrow
+
+import java.io.{File, PrintWriter}
+import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
+
+import scala.collection.mutable.{ArrayBuffer, SynchronizedBuffer}
+import scala.math
+
+import spark._
+import SparkContext._
+
+/**
+ * Launches jobs composed of sleep tasks at a fixed rate.
+ *
+ * Intended for use to test scheduler throughput.
+ */
+object ThroughputTester {
+  var responseTimes = new ArrayBuffer[Int]() with SynchronizedBuffer[Int]
+
+  def get_percentile(values: ArrayBuffer[Int], percent: Double): Double = {
+    val index = (values.size - 1) * percent
+    val below = math.floor(index).toInt
+    val above = math.ceil(index).toInt
+    if (below == above) {
+      return values(below)
+    }
+    val d0 = values(below) * (above - index)
+    val d1 = values(above) * (index - below)
+    return d0 + d1
+  }
+
+  def main(args: Array[String]) {
+    if (args.length != 5) {
+      System.err.println(
+        "Usage: ThroughputTester master tasks_per_job task_duration total_cores load")
+      System.exit(1)
+    }
+    var testBuf = new ArrayBuffer[Int]()
+    testBuf += 0
+    testBuf += 1
+    testBuf += 2
+    println("Percentile 0.8: " + get_percentile(testBuf, 0.5))
+    val tasksPerJob = args(1).toInt
+    val taskDurationMillis = args(2).toInt
+    val totalCores = args(3).toInt
+    val load = args(4).toDouble
+    val interarrivalDelay = tasksPerJob * taskDurationMillis / (totalCores * load)
+
+    val sc = new SparkContext(args(0), "ThroughputTester", System.getenv("SPARK_HOME"), Seq())
+    println("Sleeping to give everythign time to start up")
+    Thread.sleep(10000)
+    println("Done sleeping; launching warmup job")
+    sc.parallelize(1 to 100000, 100000).map(x => Thread.sleep(1)).count()
+    println("Warmup job complete")
+
+    println("Launching tasks with interarrival delay %s (to sustain load %s on %s cores)".format(
+      interarrivalDelay, load, totalCores))
+    val pool = new ScheduledThreadPoolExecutor(200)
+    val queryRunnable = new QueryLaunchRunnable(sc, tasksPerJob, taskDurationMillis)
+    pool.scheduleAtFixedRate(
+      queryRunnable, 0, (interarrivalDelay * 1000000).toLong, TimeUnit.NANOSECONDS)
+    val startTime = System.currentTimeMillis
+    println("Running experiment for 5 minutes")
+    val experimentDurationMillis = 5 * 60 * 1000
+    Thread.sleep(experimentDurationMillis)
+    pool.shutdown()
+
+    if (responseTimes.size == 0) return
+    val sortedResponseTimes = responseTimes.sortWith(_ < _)
+    val writer = new PrintWriter(new File("throughput_results_%s.txt".format(taskDurationMillis)))
+    (0 to 100).foreach { x =>
+      val percentile = x / 100.0
+      writer.write("%s\t%s\n".format(percentile, get_percentile(sortedResponseTimes, percentile)))
+    }
+    writer.close()
+  }
+}
+
+class QueryLaunchRunnable(sc: SparkContext, numTasks: Int, taskDurationMillis: Int)
+  extends Runnable with Logging {
+  def run() {
+    val startTime = System.currentTimeMillis
+    // This is a hack so that Spark doesn't try to serialize the whole QueryLaunchRunnable object.
+    val millis = taskDurationMillis
+    sc.parallelize(1 to numTasks, numTasks).map(x => Thread.sleep(millis)).count()
+    val responseTime = System.currentTimeMillis - startTime
+    logInfo("QueryTime: " + responseTime)
+    ThroughputTester.responseTimes += responseTime.toInt
+  }
+}
